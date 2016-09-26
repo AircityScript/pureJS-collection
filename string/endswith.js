557fddcc87450534978db45b86f169336b44d400
aircity <aircity@drrr.us>
2016-09-26 11:14:28 +0800

    commit

--- string/endswith.js
+++ string/endswith.js
@@ -49,3 +49,16 @@ if (!String.prototype.endsWith) {
 		}
 	}());
 }
+
+/* developer.mozila.org */
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.lastIndexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
--- string/startswith.js
+++ string/startswith.js
@@ -44,3 +44,11 @@ if (!String.prototype.startsWith) {
 		}
 	}());
 }
+
+/* developer.mozila.org */
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
